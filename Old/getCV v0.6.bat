38b80d8d57cd482ca2e07c7eb1cdc6c08a7e5482
Piotr Janczewski <piotr.janczewski@accenture.com>
2024-04-05 14:31:02 +0200

    Initial

---
+++ Old/getCV v0.6.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat & streamlit run "C:\Users\%USERNAME%\Accenture\Warsaw Analytics - Documents\01_CVs\CVapp\CVapp0_6.py" 
+PAUSE
---
+++ getCV v0.7.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+python.exe -m streamlit run "C:\Users\%USERNAME%\Accenture\Warsaw Analytics - Documents\01_CVs\CVapp\CVapp0_7.py" 
+PAUSE
